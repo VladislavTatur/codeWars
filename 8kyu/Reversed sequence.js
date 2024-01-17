5280278340f21955f2a724e8023bfc8db4fed126
Vladislav <vlad3873@mail.ru>
2024-01-17 23:12:54 +0300

    Reversed sequence

---
+++ 8kyu/Reversed sequence.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+
+// SOLUTION:
+const reverseSeq = n => {
+let newArray = [];
+for (let i = 1; i <= n; i++) {
+    newArray.push(i)
+}
+newArray.reverse()
+return newArray
+};
+
+
+  console.log(reverseSeq(5))
