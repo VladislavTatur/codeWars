2deac1ea08331bcaa53979fab52fbc84c0a16f88
Vladislav <vlad3873@mail.ru>
2024-01-24 21:53:27 +0300

    Opposite number

---
+++ 8kyu/Opposite number.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+
+
+//SOLUTION:
+function opposite(number) {
+    return number ? -number : number
+  }
+
+  console.log(opposite(-1))
